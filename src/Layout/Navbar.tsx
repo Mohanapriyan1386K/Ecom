f552c7d39e1ac11f25731ca8f04ab7cea95d7e0d
Mohanapriyan1386K <mohanapriyan.k.mfedu@gmail.com>
2025-06-17 22:11:27 +0530

    first commit

--- src/Layout/Navbar.tsx
+++ src/Layout/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState, type JSX, type JSXElementConstructor } from "react";
+import { useState, type JSX,} from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import CartIcon from "../components/CartIcon";
 
--- src/Screen/Cart.tsx
+++ src/Screen/Cart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { FaHome } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import { toast, ToastContainer } from "react-toastify";
