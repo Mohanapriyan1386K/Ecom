0e801acfecafe6d66dff090617691b576fe2b390
Mohanapriyan1386K <mohanapriyan.k.mfedu@gmail.com>
2025-06-18 18:55:06 +0530

    login

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -5,9 +5,9 @@ const CartIcon = () => {
   const cartItems = JSON.parse(localStorage.getItem('cart') || '[]');
   return (
     <div className="relative">
-      <FaShoppingCart className="text-[15px] text-white" />
+      <FaShoppingCart className="text-[15px] text-black sm:text-white" />
       {cartItems.length > 0 && (
-        <span className="absolute -top-1 -right-2 bg-red-600 text-black lg:text-white text-[9px] font-bold rounded-full w-3 h-3 flex items-center justify-center">
+        <span className="absolute -top-1 -right-2 bg-gray-900 sm:bg-red-600 text-white text-[9px] font-bold rounded-full w-3 h-3 flex items-center justify-center">
           {cartItems.length}
         </span>
       )}
